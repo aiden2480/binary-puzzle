244a375d66946119c1206d1ec63826c32245b176
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2021-05-15 15:42:35 +1000

    Add support for 6x6 puzzles

--- python/main.py
+++ python/main.py
@@ -41,8 +41,8 @@ def main(binpuz: bool):
     print(title if binpuz else "BinaryPuzzle\nAiden Gardner\n")
     
     print(f"Enter {colour.CYAN}0{colour.RESET} for a random puzzle")
-    #   print(f"Enter {colour.CYAN}6{colour.RESET} and then a digit between 1 and 13 for the corresponding 6x6 puzzle. ", end="")
-    #   print(f"{colour.GREEN}e.g.{colour.RESET} {colour.CYAN}61{colour.RESET} or {colour.CYAN}612{colour.RESET}")
+    print(f"Enter {colour.CYAN}6{colour.RESET} and then a digit between 1 and 13 for the corresponding 6x6 puzzle. ", end="")
+    print(f"{colour.GREEN}e.g.{colour.RESET} {colour.CYAN}61{colour.RESET} or {colour.CYAN}613{colour.RESET}")
     
     print(f"Enter {colour.CYAN}8{colour.RESET} for an 8x8 puzzle")
     print(f"Enter {colour.CYAN}14{colour.RESET} for a 14x14 puzzle")
@@ -73,8 +73,8 @@ def main(binpuz: bool):
         print(colour.RESET, end="")
 
         binary = BinaryPuzzle(string)
-    elif not code.isdigit():
-        print(f"{colour.RED}You must enter an integer!{colour.RESET}")
+    elif not code.isdigit() or int(code) not in [8, 14, 61, 62, 63, 64, 65, 66, 67, 68, 69, 610, 611, 612, 613]:
+        print(f"{colour.RED}You must enter a valid option!{colour.RESET}")
         time.sleep(3)
         print()
         return main(binpuz)
@@ -83,11 +83,14 @@ def main(binpuz: bool):
     
     # Load selected puzzle
     if puzid == 0:
-        binary = BinaryPuzzle.example(0)
+        binary = BinaryPuzzle.example(0)        # Random puzzle
     elif puzid == 8:
-        binary = BinaryPuzzle.example(14) # Example 8x8 puzzle
+        binary = BinaryPuzzle.example(14)       # Example 8x8 puzzle
     elif puzid == 14:
-        binary = BinaryPuzzle.example(15) # Example 8x8 puzzle
+        binary = BinaryPuzzle.example(15)       # Example 8x8 puzzle
+    elif str(puzid).startswith("6"):
+        puzid = int(str(puzid)[1:])
+        binary = BinaryPuzzle.example(puzid)    # Example 6x6 puzzle
 
     # Print initial grid
     print(f"\n{colour.GREEN}INITIAL GRID{colour.RESET}")
