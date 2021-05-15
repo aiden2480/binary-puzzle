08e88075195a9d25d56489a6b56fb8d434ccd2ba
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2021-05-15 16:31:27 +1000

    zero *is* a valid option

--- python/main.py
+++ python/main.py
@@ -73,7 +73,7 @@ def main(binpuz: bool):
         print(colour.RESET, end="")
 
         binary = BinaryPuzzle(string)
-    elif not code.isdigit() or int(code) not in [8, 14, 61, 62, 63, 64, 65, 66, 67, 68, 69, 610, 611, 612, 613]:
+    elif not code.isdigit() or int(code) not in [0, 8, 14, 61, 62, 63, 64, 65, 66, 67, 68, 69, 610, 611, 612, 613]:
         print(f"{colour.RED}You must enter a valid option!{colour.RESET}")
         time.sleep(3)
         print()
